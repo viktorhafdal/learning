9a71d0979be8475471e2f5218564cb205cf11eb4
viktorhafdal <vgerlingsson@gmail.com>
2025-10-09 18:24:52 +0200

    F# Luciens Lasagna

---
+++ fsharp/Exercism/LuciensLasagne/Luciens.fs
@@ -0,0 +1,14 @@
+﻿module LuciansLusciousLasagna
+
+// TODO: define the 'expectedMinutesInOven' binding
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let remainingMinutesInOven minutesLapsed = expectedMinutesInOven - minutesLapsed
+
+// TODO: define the 'preparationTimeInMinutes' function
+let preparationTimeInMinutes layersAdded = 2 * layersAdded
+
+// TODO: define the 'elapsedTimeInMinutes' function
+let elapsedTimeInMinutes layersAdded minutesLapsed =
+  preparationTimeInMinutes layersAdded + minutesLapsed
---
+++ fsharp/Exercism/Tests/LuciensLasagneTest/Tests.fs
@@ -0,0 +1,21 @@
+﻿module Tests
+
+open Xunit
+
+open LuciansLusciousLasagna
+
+[<Fact>]
+let ``TestExpectedMinutessInOven`` () =
+  Assert.Equal(40, expectedMinutesInOven)
+
+[<Fact>]
+let ``TestRemainingMinutesInOven`` () =
+  Assert.Equal(30, remainingMinutesInOven 10)
+
+[<Fact>]
+let ``TestPreparationTimeInOven`` () =
+  Assert.Equal(4, preparationTimeInMinutes 2)
+
+[<Fact>]
+let ``TestElapsedTimeInMinutes`` () =
+  Assert.Equal(26, elapsedTimeInMinutes 3 20)
